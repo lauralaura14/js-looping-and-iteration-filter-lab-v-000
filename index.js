46ec32c8776873ad00700564fc04f6b75db9abe7
Laura <laurao148@gmail.com>
2018-08-15 22:28:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function fuzzyMatch (drivers, letters) {
 
 function matchName (drivers, string) {
   return drivers.filter(function(driver) {
-    return driver.name.toLowerCase() === stromg.toLowerCase();
+    return driver.name.toLowerCase() === string.toLowerCase();
   })
 }
